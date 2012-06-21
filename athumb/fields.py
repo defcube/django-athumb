3f8aadd892c2f70f21ab6bcfe1bbb32835c3cd99
Jose Penarrieta <soad241@gmail.com>
2012-06-20 19:41:04 -0500

    including params

--- athumb/fields.py
+++ athumb/fields.py
@@ -52,6 +52,11 @@ class ImageWithThumbsFieldFile(ImageFieldFile):
 
         # Split URL from GET attribs.
         url_get_split = self.url.rsplit('?', 1)
+        try:
+            params = url_get_split[1]
+        except IndexError:
+            params = ""
+        
         # Just the URL string (no GET attribs).
         url_str = url_get_split[0]
         # Get the URL string without the original's filename at the end.
@@ -59,9 +64,15 @@ class ImageWithThumbsFieldFile(ImageFieldFile):
 
         # Slap the new thumbnail filename on the end of the old URL, in place
         # of the orignal image's filename.
-        new_url = "%s/%s?cbust=%s" % (url_minus_filename,
-                                      os.path.basename(new_filename),
-                                      settings.MEDIA_CACHE_BUSTER)
+        if params:
+            new_url = "%s/%s?cbust=%s&%s" % (url_minus_filename,
+                                             os.path.basename(new_filename),
+                                             settings.MEDIA_CACHE_BUSTER,
+                                             params)
+        else:
+            new_url = "%s/%s?cbust=%s" % (url_minus_filename,
+                                             os.path.basename(new_filename),
+                                             settings.MEDIA_CACHE_BUSTER)
 
         if ssl_mode:
             new_url = new_url.replace('http://', 'https://')
